592201335726ef4a9f8f4e21815425985d5c7b7b
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-19 15:17:22 +0530

    Question -1 completed

--- javascriptQuestion-1.js
+++ javascriptQuestion-1.js
@@ -1,1 +1,16 @@
 // Question 1-FIZZBUZZ
+
+const FizzBuzz = (n)=>{
+    for (let i = 0; i<=n; i++){
+        let output = ""
+        if (i%3 === 0){
+            output += "Fizz";
+        }
+        if(i%5 === 0){
+            output += "Buzz";
+        }
+        console.log(output || i);
+    }
+}
+
+FizzBuzz(20);
